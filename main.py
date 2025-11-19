47cb1e276b600f380027b1269c42b762789a4c15
SvitlanaShustur <svsh.data.analytics@gmail.com>
2025-11-19 16:13:11 +0100

    проводили очищення даних

--- main.py
+++ main.py
@@ -1,45 +1,126 @@
 import csv
 import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_columns', None)  # показувати всі стовпці
+# pd.set_option('display.max_columns', 50)  # максимальна кількість стовпців для відображення
+pd.set_option('display.width', 120)  # ширина виводу. 120 символів можуть бути виведені в одному рядку. можуть редакуватися
 
 
 ### **1. Імпорт та первинне дослідження**
 
 url = 'https://s3-eu-west-1.amazonaws.com/shanebucket/downloads/uk-500.csv' # визначаємо дані
-df = pd.read_csv(url) # завантажуємо дані у DataFrame
-# print(df) # виводимо дані на екран
+df_origin = pd.read_csv(url) # завантажуємо дані у DataFrame
+
+COLUMNS_TO_DROP = []
+# print("\n---  head ---")
+# print(df.head(5)) # виводимо перші 5 записів на екран
+
+# print("\n---  info ---")
 # df.info() # отримуємо інформацію про DataFrame
+
+# print("\n---  describe ---")
 # df.describe() # отримуємо статистичний опис даних
-# print("----")
-# print(df.isna().sum()) # перевіряємо на наявність пропущених значень
+
+# print("\n---  describe for str ---")
+# print(df.describe(include=[object]).T) # 
+
+# print("\n---  isna / null  ---")
+# print(df.isna().sum().sort_values(ascending=False).head(20)) # перевіряємо на наявність пропущених значень. Сортуємо за спаданням та виводимо перші 20
+
+# print("\n---  duplicated ---")
 # print(df.duplicated().sum()) # перевіряємо на наявність дублікатів
+
+# print("\n---  List colums 1 ---") # виводимо список стовпців
+# # print(df.columns.tolist()) # виводимо список стовпців у вигляді списку
+# list_col = df.columns # зберігаємо список стовпців у змінну
+# print(list(list_col)) # виводимо список стовпців у вигляді списку
+
+# print("\n---  List colums 2 ---") # виводимо список стовпців з індексами
+# for i, col in enumerate(df.columns): # виводимо індекс та назву кожного стовпця
+#     print(f"{i:02d}. {col}") # виводимо індекс та назву стовпця з провідними нулями
+
 
 
 ### **2. Очищення даних**
+
+#1. Видаляємо непотрібні стовпці. Універсальний варіант
+df = df_origin.copy() # створюємо копію DataFrame для збереження оригінальних даних
+if COLUMNS_TO_DROP: # якщо є стовпці для видалення
+    print("\n--- delete columns in lisr ---") # виводимо повідомлення
+    df = df.drop(colums=[col for col in COLUMNS_TO_DROP if df in df.columns], errors="ignore") # видаляємо стовпці зі списку COLUMNS_TO_DROP, якщо вони є у DataFrame
+# другий варіант запису
+# # columns = [] # створюємо порожній список для збереження назв стовпців для видалення
+# # for col in COLUMNS_TO_DROP: # ітеруємося по списку COLUMNS_TO_DROP
+# #     if col in df_raw.columns: # якщо стовпець є у DataFrame
+# # #         columns.append(col) # додаємо назву стовпця до списку columns
+else:
+    print("\n--- no columns to delete ---") # виводимо повідомлення, якщо немає стовпців для видалення
+
+# Функція для стандартизації тексту (видалення зайвих пробілів, приведення до рядка)
+def standardize_text(s): # функція для стандартизації тексту
+    if pd.isna(s): # перевіряємо на пропущене значення
+        return np.nan # повертаємо NaN, якщо значення пропущене
+    
+    if not isinstance(s, str): # перевіряємо, чи є значення рядком/-- isinstance це вбудована функція в мові програмування Python, яка перевіряє, чи є об'єкт екземпляром певного класу або одного з його підкласів
+        s = str(s) # перетворюємо значення на рядок, якщо це не так
+
+    s = s.strip() # видаляємо пробіли на початку та в кінці рядка
+    s = ' '.join(s.split()) # видаляємо зайві пробіли між словами
+
+    return s # повертаємо стандартизований / очищений рядок
+
+# Привести `email` та `web` до нижнього регістру.
+# Ідентифікуємо можливі стовпці з email, web, phone, fax
+possible_email_cols = [c for c in df.columns if "email" in c.lower()] # знаходимо всі стовпці, які містять "email" у назві
+possible_web_cols = [c for c in df.columns if ("web" in c.lower() or "website" in c.lower() or "url" in c.lower())] # знаходимо всі стовпці, які містять "web", "website" або "url" у назві
+possible_phone_cols = [c for c in df.columns if ("phone" in c.lower() or "telephon" in c.lower() or "tel" in c.lower() or "mobile" in c.lower())] # знаходимо всі стовпці, які містять "phone", "tel" або "mobile" у назві
+possible_fax_cols = [c for c in df.columns if ("fax" in c.lower())] # знаходимо всі стовпці, які містять "fax" у назві
+
+# генерація списку
+# [зміна_циклу (з приміненими операціями) for змінна_циклу in де_проходиться]
+# [0, 1, 2, 3, 4] 
+# [n for n in range(4)]
+print("\n--- possible columns ---")
+print("Email columns:", possible_email_cols) # виводимо можливі стовпці з email
+print("Web columns:", possible_web_cols) # виводимо можливі стовпці з web
+print("Phone columns:", possible_phone_cols) # виводимо можливі стовпці з phone
+print("Fax columns:", possible_fax_cols) # виводимо можливі стовпці з fax
+
+# Привести `email` та `web` до нижнього регістру.
+# Застосовуємо функцію стандартизації тексту до всіх стовпців типу object (рядок)
+for col in df.select_dtypes(include=['object']).columns: # ітеруємося по всіх стовпцях типу object
+    df[col] = df[col].apply(standardize_text) # застосовуємо функцію стандартизації тексту до кожного стовпця
+
+for col in possible_email_cols: # ітеруємося по можливих стовпцях з email
+    df[col] = df[col].str.lower() # перетворюємо всі символи на малі літери
+
+for col in possible_web_cols: # ітеруємося по можливих стовпцях з web
+    df[col] = df[col].str.lower() # перетворюємо всі символи на малі літери
+
+
+### **3. Стандартизувати формат текстових полів. ###
+
+
+### **3. Приклади очищення окремих стовпців**
+
 #### Перевіряємо записи у стовпці "email", "web", "phone1", "phone2"
-# print(df['first_name'].head(5))
-# print(df['last_name'].head(5))
-# print(df['company_name'].head(5))
-# print(df['address'].head(5))
-# print(df['city'].head(5))
-# print(df['county'].head(5))
-# print(df['postal'].head(5))
-# print(df['phone1'].head(5))
-# print(df['phone2'].head(5))
-# print(df['web'].head(5))
+# print(df['first_name'].head(5)) # виводимо перші 5 записів стовпця "first_name"
+#
 # # df = df.drop("phone2", axis=1) # видаляємо стовпець "phone2"
 # df['email'] = df['email'].str.lower()
 # df['web'] = df['web'].str.lower()
 
-df['phone1_1'] = df['phone1'].copy() # створюємо копію стовпця "phone1"
-df['phone1_1'] = df['phone1_1'].astype(str).str.replace('-', '', regex=True)    # видаляємо дефіси
-df['phone1_2'] = df['phone1_1'].copy() # створюємо копію стовпця "phone1_1"
-df['phone1_2'] = df['phone1_1'].str[1:] # видаляємо перший символ (0)
-# print(df['phone1_2'].head(5))
+# df['phone1_1'] = df['phone1'].copy() # створюємо копію стовпця "phone1"
+# df['phone1_1'] = df['phone1_1'].astype(str).str.replace('-', '', regex=True)    # видаляємо дефіси
+# df['phone1_2'] = df['phone1_1'].copy() # створюємо копію стовпця "phone1_1"
+# df['phone1_2'] = df['phone1_1'].str[1:] # видаляємо перший символ (0)
+# # print(df['phone1_2'].head(5))
 
-df['telephon'] = '+44 ' + df['phone1_2'] # додаємо код країни +44
-df['telephon'] = (
-    df['telephon'].str[:6] + ' ' +  # Беремо перші 6 символів (+441944) і додаємо пробіл
-    df['telephon'].str[6:10] + " " + # Додаємо наступні 4 символи (7700) і додаємо пробіл
-    df['telephon'].str[10:14]               # Додаємо останні 4 символи (1234)
-)  # +44 770
-print(df['telephon'].head(5))
+# df['telephon'] = '+44 ' + df['phone1_2'] # додаємо код країни +44
+# df['telephon'] = (
+#     df['telephon'].str[:6] + ' ' +  # Беремо перші 6 символів (+441944) і додаємо пробіл
+#     df['telephon'].str[6:10] + " " + # Додаємо наступні 4 символи (7700) і додаємо пробіл
+#     df['telephon'].str[10:14]               # Додаємо останні 4 символи (1234)
+# )  # +44 770
+# print(df['telephon'].head(5))
